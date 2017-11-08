e08809c8a1239945fa3a4b32650a92ef6c533f21
Taiwen Jin <kimdaxterkid@gmail.com>
2017-11-08 15:34:36 -0500

    Initial Commit

---
+++ SimpleDBA/DocumentViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DocumentViewController.swift
+//  SimpleDBA
+//
+//  Created by Taiwen Jin on 11/8/17.
+//  Copyright © 2017 Taiwen Jin. All rights reserved.
+//
+
+import UIKit
+
+class DocumentViewController: UIViewController {
+    
+    @IBOutlet weak var documentNameLabel: UILabel!
+    
+    var document: UIDocument?
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        // Access the document
+        document?.open(completionHandler: { (success) in
+            if success {
+                // Display the content of the document, e.g.:
+                self.documentNameLabel.text = self.document?.fileURL.lastPathComponent
+            } else {
+                // Make sure to handle the failed import appropriately, e.g., by presenting an error message to the user.
+            }
+        })
+    }
+    
+    @IBAction func dismissDocumentViewController() {
+        dismiss(animated: true) {
+            self.document?.close(completionHandler: nil)
+        }
+    }
+}
